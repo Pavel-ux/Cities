a94b0db2d78249ec504a483ff77082f9e5480e50
Pavel-ux <57436693+Pavel-ux@users.noreply.github.com>
2019-12-03 01:55:46 +0200

    Fix
    Changed $table->unsignedInteger('role_id');
                   $table->unsignedInteger('user_id');
    TO
                   $table->bigInteger('role_id')->unsigned();
                   $table->bigInteger('user_id')->unsigned();

--- database/migrations/2019_11_19_121722_create_user_roles_table.php
+++ database/migrations/2019_11_19_121722_create_user_roles_table.php
@@ -14,8 +14,8 @@ class CreateUserRolesTable extends Migration
     public function up()
     {
         Schema::create('user_roles', function (Blueprint $table) {
-          $table->unsignedInteger('role_id');
-          $table->unsignedInteger('user_id');
+          $table->bigInteger('role_id')->unsigned();
+          $table->bigInteger('user_id')->unsigned();
 
           $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
           $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
